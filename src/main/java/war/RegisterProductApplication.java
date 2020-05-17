88a74d95200c034527b551b52c57cdaaf2efc72c
Lucat0n <39059724+Lucat0n@users.noreply.github.com>
2020-05-17 17:01:28 +0200

    add app prototype

---
+++ src/main/java/war/RegisterProductApplication.java
@@ -0,0 +1,8 @@
+package war;
+
+import org.camunda.bpm.application.ProcessApplication;
+import org.camunda.bpm.application.impl.ServletProcessApplication;
+
+@ProcessApplication("rejestracja_produktu")
+public class RegisterProductApplication extends ServletProcessApplication {
+}
