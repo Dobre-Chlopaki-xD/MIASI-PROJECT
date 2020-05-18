56676b767c589aafe8da0637b517a2424811c189
209450 <209450@edu.p.lodz.pl>
2020-05-18 02:28:24 +0200

    Basic project configuration

--- src/main/java/war/RegisterProductApplication.java
+++
@@ -1,8 +0,0 @@
-package war;
-
-import org.camunda.bpm.application.ProcessApplication;
-import org.camunda.bpm.application.impl.ServletProcessApplication;
-
-@ProcessApplication("rejestracja_produktu")
-public class RegisterProductApplication extends ServletProcessApplication {
-}
