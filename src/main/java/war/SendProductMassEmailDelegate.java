b73066cf21d2eeccd22ced6a31c7c67dd3f80ec9
Lucat0n <39059724+Lucat0n@users.noreply.github.com>
2020-06-08 01:13:56 +0200

    fixed database mess

--- src/main/java/war/SendProductMassEmailDelegate.java
+++ src/main/java/war/SendProductMassEmailDelegate.java
@@ -3,6 +3,8 @@ package war;
 import org.camunda.bpm.engine.delegate.DelegateExecution;
 import org.camunda.bpm.engine.delegate.JavaDelegate;
 
+import java.util.Random;
+
 /**
  * This is an easy adapter implementation 
  * illustrating how a Java Delegate can be used 
@@ -11,8 +13,10 @@ import org.camunda.bpm.engine.delegate.JavaDelegate;
 public class SendProductMassEmailDelegate implements JavaDelegate {
   public void execute(DelegateExecution execution) throws Exception {
     EmailSender es = new EmailSender();
+    Random generator = new Random();
+    int mass = (int)(execution.getVariable("productMass")==null? generator.nextInt(12500) + 100 : execution.getVariable("ProductID"));
     es.sendMail("Produkt zostal zmodyfikowany",
-            "Do bazy danych został dodany nowy produkt o ID = " + execution.getVariable("ProductID") + " i masie wynoszącej " + execution.getVariable("productMass") + "g.");
+            "Do bazy danych został dodany nowy produkt o ID = " + execution.getVariable("ProductID") + " i masie wynoszącej " + mass + "g.");
 
   }
 }
